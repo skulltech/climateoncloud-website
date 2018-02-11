f344a8d8c3a56f21c2be196128ead948efa20974
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-02-12 01:41:41 +0530

    Add code for carousel of cards

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ app.secret_key = 'super secret key'
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html.clean')
 
 @app.route('/charts')
 def charts():
 	return render_template('charts.html')
 
+@app.route('/cards')
+def cards():
+	return render_template('carousel-of-cards.html')
 
 if __name__=='__main__':
     app.run()
